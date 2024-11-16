e5dc29f01d400b1e91933c279afa8e6e6a14eb2b
galia Arjhond <galiaarjhond0@gmail.com>
2024-11-16 18:12:51 +0800

    add availability in create account

---
+++ createAccount.Designer.vb
@@ -0,0 +1,404 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class createAccount
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        ComboBox2 = New ComboBox()
+        ComboBox3 = New ComboBox()
+        ComboBox4 = New ComboBox()
+        TextBox4 = New TextBox()
+        Profile = New GroupBox()
+        GroupBox2 = New GroupBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        GroupBox1 = New GroupBox()
+        ComboBox1 = New ComboBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Profile.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(31, 56)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(97, 19)
+        Label1.TabIndex = 0
+        Label1.Text = "First Name:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(31, 127)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(96, 19)
+        Label2.TabIndex = 1
+        Label2.Text = "Last Name:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(31, 199)
+        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(140, 19)
+        Label3.TabIndex = 2
+        Label3.Text = "Contact Number:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(31, 273)
+        Label4.Margin = New Padding(4, 0, 4, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(140, 19)
+        Label4.TabIndex = 3
+        Label4.Text = "License Number:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(31, 203)
+        Label5.Margin = New Padding(4, 0, 4, 0)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(92, 19)
+        Label5.TabIndex = 4
+        Label5.Text = "Password:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(31, 350)
+        Label6.Margin = New Padding(4, 0, 4, 0)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(104, 19)
+        Label6.TabIndex = 5
+        Label6.Text = "Department:"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(31, 429)
+        Label7.Margin = New Padding(4, 0, 4, 0)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(146, 19)
+        Label7.TabIndex = 6
+        Label7.Text = "Experience Level:"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(31, 60)
+        Label8.Margin = New Padding(4, 0, 4, 0)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(151, 19)
+        Label8.TabIndex = 7
+        Label8.Text = "Security Question:"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(31, 131)
+        Label9.Margin = New Padding(4, 0, 4, 0)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(139, 19)
+        Label9.TabIndex = 8
+        Label9.Text = "Security Answer:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(210, 53)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(261, 26)
+        TextBox1.TabIndex = 9
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(210, 124)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(261, 26)
+        TextBox2.TabIndex = 10
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(210, 196)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(261, 26)
+        TextBox3.TabIndex = 11
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(198, 128)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(261, 26)
+        TextBox7.TabIndex = 15
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(198, 200)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(261, 26)
+        TextBox8.TabIndex = 16
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(210, 342)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(261, 27)
+        ComboBox2.TabIndex = 18
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Location = New Point(198, 56)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(261, 27)
+        ComboBox3.TabIndex = 19
+        ' 
+        ' ComboBox4
+        ' 
+        ComboBox4.FormattingEnabled = True
+        ComboBox4.Location = New Point(210, 421)
+        ComboBox4.Name = "ComboBox4"
+        ComboBox4.Size = New Size(261, 27)
+        ComboBox4.TabIndex = 19
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(210, 266)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(261, 26)
+        TextBox4.TabIndex = 20
+        ' 
+        ' Profile
+        ' 
+        Profile.BackColor = SystemColors.ControlLight
+        Profile.Controls.Add(TextBox4)
+        Profile.Controls.Add(ComboBox4)
+        Profile.Controls.Add(ComboBox2)
+        Profile.Controls.Add(TextBox3)
+        Profile.Controls.Add(TextBox2)
+        Profile.Controls.Add(TextBox1)
+        Profile.Controls.Add(Label7)
+        Profile.Controls.Add(Label6)
+        Profile.Controls.Add(Label4)
+        Profile.Controls.Add(Label3)
+        Profile.Controls.Add(Label2)
+        Profile.Controls.Add(Label1)
+        Profile.Location = New Point(30, 26)
+        Profile.Name = "Profile"
+        Profile.Size = New Size(507, 548)
+        Profile.TabIndex = 21
+        Profile.TabStop = False
+        Profile.Text = "Profile"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.BackColor = SystemColors.ControlLight
+        GroupBox2.Controls.Add(ComboBox3)
+        GroupBox2.Controls.Add(TextBox8)
+        GroupBox2.Controls.Add(TextBox7)
+        GroupBox2.Controls.Add(Label9)
+        GroupBox2.Controls.Add(Label8)
+        GroupBox2.Controls.Add(Label5)
+        GroupBox2.Location = New Point(573, 314)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(494, 260)
+        GroupBox2.TabIndex = 22
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Security "
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(908, 613)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(159, 35)
+        Button1.TabIndex = 23
+        Button1.Text = "Create Account"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(30, 613)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(159, 35)
+        Button2.TabIndex = 24
+        Button2.Text = "Back"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(720, 613)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(159, 35)
+        Button3.TabIndex = 25
+        Button3.Text = "Clear"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = SystemColors.ControlLight
+        GroupBox1.Controls.Add(ComboBox1)
+        GroupBox1.Controls.Add(TextBox5)
+        GroupBox1.Controls.Add(TextBox6)
+        GroupBox1.Controls.Add(Label10)
+        GroupBox1.Controls.Add(Label11)
+        GroupBox1.Controls.Add(Label12)
+        GroupBox1.Location = New Point(573, 26)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(494, 260)
+        GroupBox1.TabIndex = 23
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Availability"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(198, 56)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(261, 27)
+        ComboBox1.TabIndex = 19
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(198, 200)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(261, 26)
+        TextBox5.TabIndex = 16
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(198, 128)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(261, 26)
+        TextBox6.TabIndex = 15
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(31, 131)
+        Label10.Margin = New Padding(4, 0, 4, 0)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(127, 19)
+        Label10.TabIndex = 8
+        Label10.Text = "Available From:"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(31, 60)
+        Label11.Margin = New Padding(4, 0, 4, 0)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(108, 19)
+        Label11.TabIndex = 7
+        Label11.Text = "Day of week:"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(31, 203)
+        Label12.Margin = New Padding(4, 0, 4, 0)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(106, 19)
+        Label12.TabIndex = 4
+        Label12.Text = "Available To:"
+        ' 
+        ' createAccount
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 19F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1116, 681)
+        Controls.Add(GroupBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox2)
+        Controls.Add(Profile)
+        Font = New Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(4, 4, 4, 4)
+        Name = "createAccount"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "createAccount"
+        Profile.ResumeLayout(False)
+        Profile.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Profile As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+End Class
