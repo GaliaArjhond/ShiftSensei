d19ed031f2bffe0890ce7564b80c3efb46d1730d
galia Arjhond <galiaarjhond0@gmail.com>
2024-11-22 23:41:33 +0800

    .

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ShiftSensei.Form1
+            Me.MainForm = Global.ShiftSensei.Admin_Dashboard
         End Sub
     End Class
 End Namespace
